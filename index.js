b38f11c7c1615a0087183e1a076513304434cd23
nicolas dellitalia <nicolas.dellitalia@stsaviationgroup.com>
2019-08-04 23:38:38 -0400

    happy days

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var app = require('express')();
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
-// var http = require('http').createServer(app);
-// var io = require('socket.io')(http);
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 const port = process.env.PORT || 3000;
 
@@ -13,14 +13,22 @@ app.get('/', function (req, res) {
 app.post('/api/chat', jsonParser, function (req, res) {
     console.log(req.body);
     res.send(req.body.message);
+
+    io.emit('chat message', req.body.message);
+
 });
 
-// io.on('connection', function (socket) {
-//         socket.on('disconnect', function () {
-//             console.log('a user connected');
-//                 console.log('user disconnected');
-//             });
-//         });
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+
 
 
 app.listen(port, function () {
