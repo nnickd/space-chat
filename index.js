96d16d51db9db322a0dc5d37da5623001260bb8d
nicolas dellitalia <nicolas.dellitalia@stsaviationgroup.com>
2019-08-04 23:09:01 -0400

    set post

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/api/chat', function (req, res) {
+    console.log(req.toString());
+});
+
 // io.on('connection', function (socket) {
-    //     socket.on('disconnect', function () {
-        //     console.log('a user connected');
-        //         console.log('user disconnected');
-        //     });
-        // });
-        
-// http.listen(3000, function () {
-//     console.log('listening on *:3000');
-// });
+//         socket.on('disconnect', function () {
+//             console.log('a user connected');
+//                 console.log('user disconnected');
+//             });
+//         });
+
+
 app.listen(port, function () {
     console.log(`listening on *:${port}`);
 });
